501da8e327b093b63b339ecf0ebaf98bc6964ab4
Sayandip2023 <138033617+Sayandip2023@users.noreply.github.com>
2023-07-17 15:38:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// JavaScript for the contact form submission
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get form values
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    // Perform validation (you can add more checks as needed)
+    if (name.trim() === "" || email.trim() === "" || message.trim() === "") {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    // Send form data to the server (you can replace the URL with your own endpoint)
+    var formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("message", message);
+
+    fetch("path/to/your/endpoint", {
+        method: "POST",
+        body: formData
+    })
+    .then(function (response) {
+        if (response.ok) {
+            alert("Message sent successfully!");
+            // Clear the form
+            document.getElementById("contact-form").reset();
+        } else {
+            alert("An error occurred. Please try again.");
+        }
+    })
+    .catch(function (error) {
+        console.error("Error:", error);
+        alert("An error occurred. Please try again.");
+    });
+});
